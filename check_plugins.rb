f4ba55dd6a81fc4e3c50a28c75bd3037d50a5d2f
Tim Hosey <thosey@cloudbees.com>
2025-02-11 10:03:50 -0800

    tweaks across the board for downloads and reading.

--- check_plugins.rb
+++ check_plugins.rb
@@ -7,24 +7,24 @@ require 'FileUtils'
 
 # This should be externalized somehow as a param
 file_name = 'active.txt'
-current_version = '2.401.3.4'
-target_version = '2.462.1.3'
+current_version = '2.414.1.4'
+target_version = '2.479.2.3'
 target_csv = 'plugins.csv'
 
 # Downloading files
-def http_download_uri(uri, filename)
+uri, filename)
   puts "Starting HTTP download for: " + uri.to_s
-  File.open(filename.to_s, "wb") do |saved_file|
+filename.to_s, "wb") do |saved_file|
     # the following "open" is provided by open-uri
-    open(uri.to_s, "rb") do |read_file|
-      saved_file.write(read_file.read)
+uri.to_s, "rb") do |read_file|
+read_file.read)
     end
   end
   puts "Stored download as " + filename + "."
 end
 
 # Validate whether or not it's in CAP
-def check_if_cap(plugin_name, json)
+plugin_name, json)
   begin
     checker = json["offeredEnvelope"]["plugins"][plugin_name]["artifactId"]
     return true
@@ -34,36 +34,36 @@ def check_if_cap(plugin_name, json)
 end
 
 # Downloads JSON files from CB UC
-def download_json(version)
+version)
   # Check to see if we've already downloaded this JSON; download if not
-  unless File.exists?("uc-#{version}.json")
-    http_download_uri("https://jenkins-updates.cloudbees.com/update-center.json?version=#{version}", "uc-#{version}.json")
-    trimmed = system('sed', '-i', 'uc.bak', '1d;$d', "uc-#{version}.json")
+"uc-#{version}.json")
+"https://jenkins-updates.cloudbees.com/update-center.json?version=#{version}", "uc-#{version}.json")
+'sed', '-i', 'uc.bak', '1d;$d', "uc-#{version}.json")
   else
     puts "Skipping download of JSON for version #{version} as it already has been downloaded."
   end 
 end
 
-def add_note(notes, new_note)
+notes, new_note)
   notes = notes.empty? ? new_note : "#{notes}\n#{new_note}"
   puts new_note
   return notes
 end
 
 # Download the target version JSON
-download_json(target_version)
+target_version)
 # Download the JSON for the current version
-download_json(current_version)
+current_version)
 
 # Read the JSON files
-target_json = JSON.parse(File.read("uc-#{target_version}.json"))
-current_json = JSON.parse(File.read("uc-#{current_version}.json"))
+"uc-#{target_version}.json"))
+"uc-#{current_version}.json"))
 
 # Delete the CSV before starting.
-File.delete(target_csv) if File.exist?(target_csv)
+target_csv)
 
 # Open CSV for writing
-csv = CSV.open(target_csv, "w")
+target_csv, "w")
 
 # CSV headers
 headers = ["plugin_id", "installed_ver", "new_ver_#{current_version}", "new_ver_#{target_version}", "req_core_#{target_version}", "cap_#{current_version}", "cap_#{target_version}", "in_uc_#{current_version}", "in_uc_#{target_version}", "notes"]
@@ -72,7 +72,7 @@ headers = ["plugin_id", "installed_ver", "new_ver_#{current_version}", "new_ver_
 csv << headers
 
 # Get file content to string
-plugin_list = File.read(file_name)
+file_name)
 
 # Set our plugin_info array to empty
 plugin_info = []
@@ -99,7 +99,7 @@ plugin_list.each_line do |plugin|
   notes = ""
 
   # Split each entry into two parts from the colon
-  p_split = plugin.split(":")
+":")
   p_id = p_split[0]
   p_ver = p_split[1]
 
@@ -109,7 +109,7 @@ plugin_list.each_line do |plugin|
     target_required_core = target_json["plugins"][p_id]["requiredCore"]
   rescue => e
     temp_note = "[#{target_version}][NOT_IN_UC] Plugin #{p_id} not found in Update Center for #{target_version}."
-    notes = add_note(notes, temp_note)
+notes, temp_note)
     in_uc_target = false
     continue_target = false
   end
@@ -120,7 +120,7 @@ plugin_list.each_line do |plugin|
     current_required_core = current_json["plugins"][p_id]["requiredCore"]
   rescue => e
     temp_note = "[#{current_version}][NOT_IN_UC] Plugin #{p_id} not found in Update Center for #{current_version}."
-    notes = add_note(notes, temp_note)
+notes, temp_note)
     in_uc_current = false
     continue_current = false
   end
@@ -130,7 +130,7 @@ plugin_list.each_line do |plugin|
     target_plugin_ver = target_json["offeredEnvelope"]["plugins"][p_id]["version"]
   rescue => e
     temp_note = "[#{target_version}][NOT_IN_CAP] Plugin #{p_id} not found in CloudBees Assurance Program for #{target_version}."
-    notes = add_note(notes, temp_note)
+notes, temp_note)
     in_cap_target = false
   end
 
@@ -139,7 +139,7 @@ plugin_list.each_line do |plugin|
     current_plugin_ver = current_json["offeredEnvelope"]["plugins"][p_id]["version"]
   rescue => e
     temp_note = "[#{current_version}][NOT_IN_CAP] Plugin #{p_id} not found in CloudBees Assurance Program for #{current_version}."
-    notes = add_note(notes, temp_note)
+notes, temp_note)
     in_cap_current = false
   end
 
@@ -147,14 +147,14 @@ plugin_list.each_line do |plugin|
   # Start analysis of versions and cores
   # ====================================
 
-  if (continue_current)
-    current_ver_diff = (current_plugin_ver.to_s > p_ver.chomp.to_s) ? current_plugin_ver.to_s : "Up-to-Date"
+continue_current)
+current_plugin_ver.to_s > p_ver.chomp.to_s) ? current_plugin_ver.to_s : "Up-to-Date"
   end
 
-  if (continue_target)
-    target_ver_diff = (target_plugin_ver.to_s > p_ver.chomp.to_s) ? target_plugin_ver.to_s : "Up-to-Date"
-    if (target_required_core < "2.277.2.1")
-      notes = add_note(notes, "Target Version required Core pre-dates Tables-to-DIVs change. Check this plugin for compatibility!")
+continue_target)
+target_plugin_ver.to_s > p_ver.chomp.to_s) ? target_plugin_ver.to_s : "Up-to-Date"
+target_required_core < "2.277.2.1")
+notes, "Target Version required Core pre-dates Tables-to-DIVs change. Check this plugin for compatibility!")
     end
   end
 
--- check_plugins_single.rb
+++ check_plugins_single.rb
@@ -7,23 +7,23 @@ require 'FileUtils'
 
 # This should be externalized somehow as a param
 file_name = 'active.txt'
-target_version = '2.462.1.3'
-target_csv = 'plugins.csv'
+target_version = '2.401.3.4'
+target_csv = 'plugins_single_target.csv'
 
 # Downloading files
-def http_download_uri(uri, filename)
+uri, filename)
   puts "Starting HTTP download for: " + uri.to_s
-  File.open(filename.to_s, "wb") do |saved_file|
+filename.to_s, "wb") do |saved_file|
     # the following "open" is provided by open-uri
-    open(uri.to_s, "rb") do |read_file|
-      saved_file.write(read_file.read)
+uri.to_s, "rb") do |read_file|
+read_file.read)
     end
   end
   puts "Stored download as " + filename + "."
 end
 
 # Validate whether or not it's in CAP
-def check_if_cap(plugin_name, json)
+plugin_name, json)
   begin
     checker = json["offeredEnvelope"]["plugins"][plugin_name]["artifactId"]
     return true
@@ -33,33 +33,33 @@ def check_if_cap(plugin_name, json)
 end
 
 # Downloads JSON files from CB UC
-def download_json(version)
+version)
   # Check to see if we've already downloaded this JSON; download if not
-  unless File.exists?("uc-#{version}.json")
-    http_download_uri("https://jenkins-updates.cloudbees.com/update-center.json?version=#{version}", "uc-#{version}.json")
-    trimmed = system('sed', '-i', 'uc.bak', '1d;$d', "uc-#{version}.json")
+"uc-#{version}.json")
+"https://jenkins-updates.cloudbees.com/update-center.json?version=#{version}", "uc-#{version}.json")
+'sed', '-i', 'uc.bak', '1d;$d', "uc-#{version}.json")
   else
     puts "Skipping download of JSON for version #{version} as it already has been downloaded."
   end 
 end
 
-def add_note(notes, new_note)
+notes, new_note)
   notes = notes.empty? ? new_note : "#{notes}\n#{new_note}"
   puts new_note
   return notes
 end
 
 # Download the target version JSON
-download_json(target_version)
+target_version)
 
 # Read the JSON files
-target_json = JSON.parse(File.read("uc-#{target_version}.json"))
+"uc-#{target_version}.json"))
 
 # Delete the CSV before starting.
-File.delete(target_csv) if File.exist?(target_csv)
+target_csv)
 
 # Open CSV for writing
-csv = CSV.open(target_csv, "w")
+target_csv, "w")
 
 # CSV headers
 headers = ["plugin_id", "installed_ver", "new_ver_#{target_version}", "req_core_#{target_version}", "cap_#{target_version}", "in_uc_#{target_version}", "notes"]
@@ -68,7 +68,7 @@ headers = ["plugin_id", "installed_ver", "new_ver_#{target_version}", "req_core_
 csv << headers
 
 # Get file content to string
-plugin_list = File.read(file_name)
+file_name)
 
 # Set our plugin_info array to empty
 plugin_info = []
@@ -90,7 +90,7 @@ plugin_list.each_line do |plugin|
   notes = ""
 
   # Split each entry into two parts from the colon
-  p_split = plugin.split(":")
+":")
   p_id = p_split[0]
   p_ver = p_split[1]
 
@@ -100,7 +100,7 @@ plugin_list.each_line do |plugin|
     target_required_core = target_json["plugins"][p_id]["requiredCore"]
   rescue => e
     temp_note = "[#{target_version}][NOT_IN_UC] Plugin #{p_id} not found in Update Center for #{target_version}."
-    notes = add_note(notes, temp_note)
+notes, temp_note)
     in_uc_target = false
     continue_target = false
   end
@@ -110,17 +110,17 @@ plugin_list.each_line do |plugin|
     target_plugin_ver = target_json["offeredEnvelope"]["plugins"][p_id]["version"]
   rescue => e
     temp_note = "[#{target_version}][NOT_IN_CAP] Plugin #{p_id} not found in CloudBees Assurance Program for #{target_version}."
-    notes = add_note(notes, temp_note)
+notes, temp_note)
     in_cap_target = false
   end
 
   # ====================================
   # Start analysis of versions and cores
   # ====================================
-  if (continue_target)
-    target_ver_diff = (target_plugin_ver.to_s > p_ver.chomp.to_s) ? target_plugin_ver.to_s : "Up-to-Date"
-    if (target_required_core < "2.277.2.1")
-      notes = add_note(notes, "Target Version required Core pre-dates Tables-to-DIVs change. Check this plugin for compatibility!")
+continue_target)
+target_plugin_ver.to_s > p_ver.chomp.to_s) ? target_plugin_ver.to_s : "Up-to-Date"
+target_required_core < "2.277.2.1")
+notes, "Target Version required Core pre-dates Tables-to-DIVs change. Check this plugin for compatibility!")
     end
   end
 
